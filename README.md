5d8adb4090d32efffb460f70b0610e9dfe554906
chrysilla mayasari <chrysilla_mysr@hotmail.com>
2021-03-26 13:48:00 +0700

    setup init
    - yarn
    - webpack

---
+++ README.md
@@ -0,0 +1,9 @@
+# Journey
+
+## Setup webpack
+https://webpack.js.org/guides/getting-started/
+1. `yarn init`
+1. `yarn add -D webpack webpack-cli`
+1. `touch webpack.config.js`
+1. `yarn add -D html-webpack-plugin`
+1. `yarn add -D webpack-dev-server` : setup simple web server for live reloading
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  devtool: 'inline-source-map', // using source maps
+  devServer: {
+    contentBase: './dist', // serve the files from dist directory
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Journey',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // clean up /dist folder
+  },
+};
