147ea7a812286747a2323d15aea15ff04dc32440
chrysilla mayasari <chrysilla_mysr@hotmail.com>
2021-04-13 17:02:36 +0700

    add sass-loader

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 # Journey
 
 ## Setup webpack
+## 🎯 Action Items:
+1. prepare webpack: livereload, css (scss, autoprefix), js
+1. develop using: atomic css
+1. watch one line layout video
+1. implement css purge, minify, uglify
+1. compare css after implementing purge, minify, uglify
+1. implement stylelint (optional)
+
+
+## 🧩 Resources:
+https://1linelayouts.glitch.me/
+
+### UI Design
+https://www.uidesigndaily.com/
+https://www.uidesigndaily.com/posts/figma-landing-page-website-day-955
+https://www.uidesigndaily.com/posts/sketch-website-card-day-1270
+https://www.uidesigndaily.com/posts/sketch-cards-list-ui-design-website-day-1218
+https://www.uidesigndaily.com/posts/figma-customers-projects-project-management-card-invoice-day-518
+https://www.uidesigndaily.com/posts/figma-stats-cards-statistics-profile-day-1141
+https://www.uidesigndaily.com/posts/figma-people-list-card-day-1310
+https://www.uidesigndaily.com/posts/sketch-calendar-components-tab-event-card-day-1211
+
+## 🔧 Setup webpack
 https://webpack.js.org/guides/getting-started/
 1. `yarn init`
 1. `yarn add -D webpack webpack-cli`
@@ -9,3 +32,4 @@ https://webpack.js.org/guides/getting-started/
 1. `yarn add -D webpack-dev-server` : setup simple web server for live reloading
 1. `yarn add -D express webpack-dev-middleware` : (optonal) setup simple web server with express
 1. `yarn add -D style-loader css-loader`
+1. `yarn add -D sass-loader sass` : https://webpack.js.org/loaders/sass-loader/
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,30 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const capitalize = (str) => {
+  // ex: landing-page -> Landing Page
   return str.split('-')
     .map(word => word.charAt(0).toUpperCase() + word.slice(1))
     .join(' ');
 }
 
-let htmlPageNames = ['test'];
+const camelCase = (str) => {
+  // ex: landing-page -> landingPage
+  return str.split('-')
+    .map((word, index) => {
+      return index !== 0
+      ? word.charAt(0).toUpperCase() + word.slice(1)
+      : word.toLowerCase()
+    })
+    .join('');
+}
+
+let htmlPageNames = ['test', 'landing-page'];
 let multipleHtmlPlugins = htmlPageNames.map(page => {
   return new HtmlWebpackPlugin({
     title: `${capitalize(page)}`,
     filename: `${page}.html`,
     template: `src/pages/${page}.html`,
-    chunks: [`${page}`] // include only certain chunks
+    chunks: [`${camelCase(page)}`] // include only certain chunks
   })
 })
 
@@ -21,13 +33,15 @@ module.exports = {
   mode: 'development',
   entry: {
     index: './src/scripts/index.js',
-    test: './src/scripts/test.js'
+    test: './src/scripts/test.js',
+    landingPage: './src/scripts/landing-page.js'
   },
   devtool: 'inline-source-map', // using source maps
   devServer: {
     contentBase: './dist', // serve the files from dist directory
     // compress: true, // enable gzip compression
     port: 3300,
+    hot: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -39,14 +53,21 @@ module.exports = {
   output: {
     filename: '[name].[contenthash].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true, // clean up /dist folder
+    clean: true, // clean up /dist folder after each build
     publicPath: '/', // will be used within our server script
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
     ],
   },
